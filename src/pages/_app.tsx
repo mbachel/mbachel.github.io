ab9451d4355c59299e03c76f98fd9d74804ff37b
mbachel <mattbachscouts@gmail.com>
2025-11-08 20:37:05 -0500

    feat: implementing page transitions

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
 import { usePathname } from 'next/navigation';
+import { AnimatePresence, motion } from 'framer-motion';
 import "../app/globals.css";
 import Navbar from '@/components/Navbar';
 import Hero from '@/components/Hero';
@@ -10,19 +11,34 @@ import Footer from '@/components/Footer';
 import Providers from '@/components/Providers';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
+  const pathname = usePathname();
+
   return (
     <Providers>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
       <Navbar />
-      {usePathname() === "/" 
-          ? <Hero /> 
-          : <MiniHero />
-      }
-      <main className="m-auto w-full max-w-6xl bg-[var(--primary-background)] shadow-lg">
-        <Component {...pageProps} />
-      </main>
+      <AnimatePresence mode="wait" initial={false}>
+          <motion.div
+            key={pathname}
+            initial={{ opacity: 0, y: 10 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: -10 }}
+            transition={{ duration: 0.2 }}
+          >
+            {pathname === "/" 
+                ? <Hero /> 
+                : <MiniHero />
+            }
+
+            <main className="m-auto w-full max-w-6xl bg-[var(--primary-background)] shadow-lg">
+              
+                  <Component {...pageProps} />
+            </main>
+            </motion.div>
+        </AnimatePresence>
       <Footer />
     </Providers>
   );
