27439db936af4eb936140cc6aa54bf6fd6d76f53
mbachel <mattbachscouts@gmail.com>
2025-11-12 17:57:02 -0500

    fix: fixing an issue from changes in previous chore, adding transition for colors

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -5,7 +5,7 @@ export default function Resume() {
     <section id="resume" className="flex items-center justify-center pt-20 pb-20 pointer-events-none">
         <a  href="/Matthew Bachelder.pdf"
             download
-            className="flex items-center justify-center text-2xl w-70 h-20 rounded-lg pointer-events-auto" 
+            className="flex items-center justify-center text-2xl w-70 h-20 rounded-lg pointer-events-auto transition" 
             id="download-resume">
                 Download Resume <FaFileDownload className="ml-3"/>
         </a>
