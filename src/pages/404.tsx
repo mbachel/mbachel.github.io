52a24004315af903273c12642095354425b6cf54
mbachel <mattbachscouts@gmail.com>
2025-11-12 17:57:22 -0500

    feat: giving 404 page better formatting

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,3 +1,22 @@
+import Link from "next/link";
+import { FaHome } from "react-icons/fa";
+
 export default function Custom404() {
-  return <h1>404 - Page Not Found</h1>
+  return (
+      <div className="flex flex-col items-center h-screen">
+        <h1 className="pt-20 text-4xl font-bold">
+          404 - Page Not Found
+        </h1>
+        <p className="pt-10 pb-20 text-2xl">
+          The page you are looking for does not exist. <br />
+          Please check the URL or return to the homepage:
+        </p>
+        <Link
+          href="/"
+          className="flex items-center justify-center text-2xl w-70 h-20 bg-(--primary-accent) rounded-lg hover:bg-(--secondary-accent) transition"
+        >
+           Home <FaHome className="ml-2" />
+        </Link>
+      </div>
+  );
 }
