ac3e7924388c04a8ba248073bc37e8704208d643
mbachel <mattbachscouts@gmail.com>
2026-01-29 14:55:32 -0500

    feat: updating contact form/page to improve mobile UX

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -11,16 +11,16 @@ export default function contact() {
             </Head>
             <h1 className="flex items-center justify-center text-5xl pt-20" id="contact">Contact</h1>
             <Resume />
-            <section id="info">
+            <section id="info" className="mx-auto">
                 <div>
                     <h2 className="text-3xl text-center pb-10">Let&apos;s Connect!</h2>
-                    <p className="text-xl text-center max-w-160 mx-auto">
+                    <p className="text-xl text-center mx-4 md:mx-40">
                         I&apos;m always excited to connect with new people, whether it&apos;s for
                         potential job opportunities, collaborations, or just to chat about tech
                         and innovation. Feel free to reach out through the contact form below
                         or connect with me on LinkedIn. I look forward to hearing from you!
                     </p>
-                    <p className="text-xl text-center max-w-160 mx-auto pt-5">
+                    <p className="text-xl text-center mx-4 md:mx-40 pt-5">
                         If you want to send me an email directly, you can reach me at:<br />
                         <a href="mailto:matthew@bachelder.me" className="text-blue-500 underline ml-1">
                             matthew@bachelder.me
